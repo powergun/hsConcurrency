67aff81202ab1068e5bbf074ac7f946849de61c6
Wei Ning <macgnw@gmail.com>
2019-08-13 22:26:27 +1000

    wip

--- par/app/parMap/Main.hs
+++ par/app/parMap/Main.hs
@@ -1,6 +1,30 @@
 
 import           Control.Parallel.Strategies hiding (parMap)
 
+-- threadscope
+-- para and con haskell L708
+{-
+install threadscope:
+brew install gtk+ gtk-mac-integration
+then download the prebuilt binary for mac os
+
+1. in package.yaml, modify ghc-options:
+      - -threaded
+      - -rtsopts
+      - -with-rtsopts=-N
+      - -eventlog
+
+2. run stack build
+stack build --executable-profiling
+
+3. execute the binary
+.stack-work/dist/x86_64-osx/Cabal-2.4.0.1/build/par-map/par-map +RTS -l
+NOTE -p will generate the prof file which can't be used here
+-l will generate eventlog
+
+4. open threadscope and open the eventlog file
+-}
+
 parMap :: (a -> b) -> [a] -> Eval [b]
 parMap _ [] = return []
 parMap f (x:xs) = do
@@ -15,6 +39,6 @@ f n | n <= 0 = 0
 
 main :: IO ()
 main = do
-  let workloads = [36, 37, 38, 39, 40, 41, 40, 39, 38, 37, 40]
+  let workloads = [10..20] ++ [35, 36, 37]
       results = runEval (parMap f workloads)
   print results
